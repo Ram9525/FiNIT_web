1e49b37354b4a755f41972dd1421d57dd3a75ad1
Ram KUMAR <ramkumar18092005@gmail.com>
2025-01-01 16:11:40 +0530

    minor update

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Card.css'
-const Card = ({title, poweredBy}) => {
+const Card = ({title, poweredBy,image}) => {
   return (
     /* From Uiverse.io by AyuuLima */ 
-<div className="EventCard">
+<div className="EventCard"
+ style={{
+  backgroundImage: `url(${image})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+}}>
   <p className="heading">{title}</p>
   <p>Powered By</p>
   <p>{poweredBy}</p>
